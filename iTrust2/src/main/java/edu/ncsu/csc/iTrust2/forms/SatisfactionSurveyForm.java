dc8e96b2ea3b6dcf7ec7cf5bce00874b3be0c821
Lucy Agamaite <laagamai@ncsu.edu>
2021-03-28 16:24:44 -0400

    finished service for satisfaction survey

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/forms/SatisfactionSurveyForm.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/forms/SatisfactionSurveyForm.java
@@ -2,11 +2,15 @@ package edu.ncsu.csc.iTrust2.forms;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import edu.ncsu.csc.iTrust2.persistant.SatisfactionSurvey;
+
 /**
  * Documents a Satisfaction survey in a form object and converts it to
  * Satusfaction
  *
- * @author lucya
+ * @author laagamai
  *
  */
 public class SatisfactionSurveyForm implements Serializable {
@@ -14,4 +18,147 @@ public class SatisfactionSurveyForm implements Serializable {
      * Serial Version of the Form. For the Serializable
      */
     private static final long serialVersionUID = 1L;
+    /** id for survey form */
+    private String            id;
+    /** waiting room time */
+    private int               timeWaitedWaitingRoom;
+    /** exam room time */
+    private int               timeWaitedExaminationRoom;
+    /** satisfaction office visit rating */
+    private int               satisfiedOfficeVisit;
+    /** treatment rating */
+    private int               satisfiedTreatment;
+
+    /** notes from survey */
+    @NotEmpty
+    private String            notes;
+
+    /**
+     * Constructor for SS form
+     *
+     * @param ss
+     *            satisfaction survey
+     */
+    public SatisfactionSurveyForm ( final SatisfactionSurvey ss ) {
+        setNotes( ss.getNotes() );
+        setId( ss.getId().toString() );
+        setSatisfiedOfficeVisit( ss.getSatisfiedOfficeVisit() );
+        setSatisfiedTreatment( ss.getSatisfiedTreatment() );
+        setTimeWaitedExaminationRoom( ss.getTimeWaitedExaminationRoom() );
+        setTimeWaitedWaitingRoom( ss.getTimeWaitedWaitingRoom() );
+    }
+
+    /**
+     * getter for id
+     *
+     * @return the id
+     */
+    public String getId () {
+        return id;
+    }
+
+    /**
+     * setter for id
+     *
+     * @param id
+     *            the id to set
+     */
+    public void setId ( final String id ) {
+        this.id = id;
+    }
+
+    /**
+     * getter for timed waiting room
+     *
+     * @return the timeWaitedWaitingRoom
+     */
+    public int getTimeWaitedWaitingRoom () {
+        return timeWaitedWaitingRoom;
+    }
+
+    /**
+     * setter for timed waiting room
+     *
+     * @param timeWaitedWaitingRoom
+     *            the timeWaitedWaitingRoom to set
+     */
+    public void setTimeWaitedWaitingRoom ( final int timeWaitedWaitingRoom ) {
+        this.timeWaitedWaitingRoom = timeWaitedWaitingRoom;
+    }
+
+    /**
+     * getter for timed exam room
+     *
+     * @return the timeWaitedExaminationRoom
+     */
+    public int getTimeWaitedExaminationRoom () {
+        return timeWaitedExaminationRoom;
+    }
+
+    /**
+     * setter for timed exam room
+     *
+     * @param timeWaitedExaminationRoom
+     *            the timeWaitedExaminationRoom to set
+     */
+    public void setTimeWaitedExaminationRoom ( final int timeWaitedExaminationRoom ) {
+        this.timeWaitedExaminationRoom = timeWaitedExaminationRoom;
+    }
+
+    /**
+     * getter for rating office visit
+     *
+     * @return the satisfiedOfficeVisit
+     */
+    public int getSatisfiedOfficeVisit () {
+        return satisfiedOfficeVisit;
+    }
+
+    /**
+     * setter for rating office visit
+     *
+     * @param satisfiedOfficeVisit
+     *            the satisfiedOfficeVisit to set
+     */
+    public void setSatisfiedOfficeVisit ( final int satisfiedOfficeVisit ) {
+        this.satisfiedOfficeVisit = satisfiedOfficeVisit;
+    }
+
+    /**
+     * getter for rating treatment
+     *
+     * @return the satisfiedTreatment
+     */
+    public int getSatisfiedTreatment () {
+        return satisfiedTreatment;
+    }
+
+    /**
+     * setter for rating treatment
+     *
+     * @param satisfiedTreatment
+     *            the satisfiedTreatment to set
+     */
+    public void setSatisfiedTreatment ( final int satisfiedTreatment ) {
+        this.satisfiedTreatment = satisfiedTreatment;
+    }
+
+    /**
+     * getter for notes
+     *
+     * @return the notes
+     */
+    public String getNotes () {
+        return notes;
+    }
+
+    /**
+     * setter for notes
+     *
+     * @param notes
+     *            the notes to set
+     */
+    public void setNotes ( final String notes ) {
+        this.notes = notes;
+    }
 }
--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/services/SatisfactionSurveyService.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/services/SatisfactionSurveyService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import edu.ncsu.csc.iTrust2.forms.SatisfactionSurveyForm;
 import edu.ncsu.csc.iTrust2.models.User;
 import edu.ncsu.csc.iTrust2.persistant.SatisfactionSurvey;
 import edu.ncsu.csc.iTrust2.repositories.SatisfactionSurveyRepository;
@@ -30,9 +31,21 @@ public class SatisfactionSurveyService extends Service {
         return repository;
     }
 
-    // public SatisfactionSurvey build () {
-    //
-    // }
+    public SatisfactionSurvey build ( final SatisfactionSurveyForm ssf ) {
+        final SatisfactionSurvey ss = new SatisfactionSurvey();
+        if ( ssf.getId() != null ) {
+            ss.setId( Long.parseLong( ssf.getId() ) );
+        }
+        ss.setTimeWaitedWaitingRoom( ssf.getTimeWaitedWaitingRoom() );
+        ss.setTimeWaitedExaminationRoom( ssf.getTimeWaitedExaminationRoom() );
+        ss.setSatisfiedOfficeVisit( ssf.getSatisfiedOfficeVisit() );
+        ss.setSatisfiedTreatment( ssf.getSatisfiedTreatment() );
+        if ( ssf.getNotes() != null ) {
+            ss.setNotes( ssf.getNotes() );
+        }
+        return ss;
+
+    }
 
     public List<SatisfactionSurvey> findByHcp ( final User hcp ) {
         return repository.findByHcp( hcp );
